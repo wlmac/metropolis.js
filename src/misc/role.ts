a1e2da751ee0cf1fbc3cc62486520d3538d30c68
Ken Shibata <kenxshibata@gmail.com>
2022-06-24 00:32:50 -0400

    fix Role

--- src/misc/role.ts
+++ src/misc/role.ts
@@ -1,11 +1,11 @@
 enum Role {
-	ProjectManager,
-	Frontend,
-	Backend,
-	App,
-    Graphics,
-    Content,
-    Extras
+	ProjectManager = "Project Manager",
+	Frontend = "Frontend Developer",
+	Backend = "Backend Developer",
+	App = "App Developer",
+	Graphics = "Graphic Designer",
+	Content = "Content Creator",
+	Game = "Game Developer",
 }
 
 export default Role;
