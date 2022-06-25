af89b1204fd12a30baa137d5b850b18994ce34cb
Ken Shibata <kenxshibata@gmail.com>
2022-06-25 00:44:44 -0400

    move user to a manager impl

--- src/client/client.ts
+++ src/client/client.ts
@@ -3,37 +3,6 @@ import Routes from '../misc/routes';
 import SessionManager, { RequestMethod, APIResponse } from '../managers/session';
 import User from '../interfaces/user';
 import Ref from '../interfaces/ref';
-import RefOrganization from './reforganization';
-import RefTag from './reftag';
-
-/**
- * UserRaw type as defined in {@link https://noi.nyiyui.ca/k/1063/4604#Get_User}.
- */
-type UserRaw = {
-  id: number;
-  username: string;
-  first_name: string;
-  last_name: string;
-  bio: string;
-  timezone: string;
-  graduating_year: string;
-  organizations: number[];
-  tags_following: number[];
-};
-
-/** Converts {@link UserRaw} to {@link User}. */
-function toUser(r: UserRaw): User {
-  return {
-    id: r.id,
-    slug: r.username,
-    name: [r.first_name, r.last_name],
-    bio: r.bio,
-    timezone: r.timezone,
-    graduatingYear: parseInt(r.graduating_year, 10),
-    organizations: r.organizations.map((i) => new RefOrganization(i)),
-    following: r.tags_following.map((i) => new RefTag(i)),
-  };
-}
 
 export default class Client {
   private session: SessionManager = new SessionManager();
@@ -45,15 +14,5 @@ export default class Client {
 
   login(username: string, password: string) {
     return this.session.login(username, password);
-  }
-
-  getUser(username: string): Promise<APIResponse<User>> {
-    return this.session
-      .request<UserRaw>(`${Routes.USER}/${encodeURI(username)}`, undefined, RequestMethod.GET, true, true)
-      .then((resp) => {
-        if (!resp.success) throw new TypeError(resp.error === null ? '' : resp.error);
-        if (resp.response === null) throw new TypeError('unreacheable');
-        return new APIResponse(toUser(resp.response));
-      });
   }
 }
